b08cb35bc469627a949f6a32a26e1d0523315792
Alex <sss0991@gmail.com>
2019-02-11 20:40:36 +1000

    docker-v2

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+VERSION = '0.0'
+
+
+setup(
+    name='simple_server',
+    version=VERSION,
+    description='return sha512',
+    author='noname',
+    author_email='noname@nomame.no',
+    platforms='any',
+    install_requires=[],
+    packages=['server'],
+)
